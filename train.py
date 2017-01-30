16539a24ff76c206d5089b1ce74c1658c939c307
lpigou <lionelpigou@gmail.com>
2017-01-30 17:44:30 +0100

    bn_gn

--- train.py
+++ train.py
@@ -150,6 +150,7 @@ def train_model(expid):
     if hasattr(config, "print_gradnorm") and config.print_gradnorm:
         all_grads = theano.grad(train_loss_theano, all_params, disconnected_inputs='warn')
         grad_norm = T.sqrt(T.sum([(g ** 2).sum() for g in all_grads]) + 1e-9)
+        theano_printer.print_me_this("grad_norm", grad_norm)
 
 
     # Compile the Theano function of your model+objective
@@ -309,9 +310,6 @@ def train_model(expid):
         for loss_name, loss in zip(train_losses_theano.keys(), mean_train_loss):
             losses[TRAINING][loss_name].append(loss)
             print string.rjust(loss_name+":",15), "%.6f" % loss
-
-        if hasattr(config, "print_gradnorm") and config.print_gradnorm:
-            print "gradient norm:", grad_norm
 
         # Now, we will do validation. We do this about every config.epochs_per_validation epochs.
         # We also always validate at the end of every training!
