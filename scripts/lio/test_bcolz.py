84fefd1dcdd6cebde6149801f4a0b708390754c5
lpigou <lionelpigou@gmail.com>
2017-01-30 19:12:09 +0100

    test bcolz

--- scripts/lio/test_bcolz.py
+++ scripts/lio/test_bcolz.py
@@ -1,4 +1,12 @@
 import bcolz
 from glob import glob
 
-DIR = "/mnt/"
+DIR = "/data/dsb3/stage1+luna_bcolz/"
+
+files = glob(DIR+"*")
+print len(files)
+
+for f in files:
+    print f
+    a = bcolz.open(f)[:]
+    print a.shape, a.dtype
