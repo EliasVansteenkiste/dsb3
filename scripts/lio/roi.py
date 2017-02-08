aba81131bbefb813ac2f21659fd3fa61587759e3
lpigou <lionelpigou@gmail.com>
2017-02-08 12:54:07 +0100

    roi

--- scripts/lio/roi.py
+++ scripts/lio/roi.py
@@ -14,7 +14,7 @@ import itertools
 import lasagne
 import time
 
-sys.path.append("../..")
+sys.path.append(".")
 from theano_utils import theano_printer
 import utils
 from utils import LOGS_PATH, MODEL_PATH, MODEL_PREDICTIONS_PATH
