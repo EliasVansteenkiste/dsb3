57c43087f8182ff058cbafb0cbd5e434b10857f3
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-02-11 23:31:44 +0100

    adding configs for false positive reduction network

---
+++ configs_luna_direct/luna_direct_v7.py
@@ -0,0 +1,174 @@
+import numpy as np
+import data_transforms
+import data_iterators
+import pathfinder
+import lasagne as nn
+from collections import namedtuple
+from functools import partial
+import lasagne.layers.dnn as dnn
+import lasagne
+import theano.tensor as T
+import utils
+import nn_lung
+
+restart_from_save = True
+restart_from_file = '/home/eavsteen/dsb3/storage/metadata/dsb3//models/eavsteen/luna_direct_v7-20170209-104801.pkl'
+rng = np.random.RandomState(33)
+
+# transformations
+p_transform = {'patch_size': (64, 64, 64),
+               'mm_patch_size': (64, 64, 64),
+               'pixel_spacing': (1., 0.7, 0.7)
+               }
+p_transform_augment = {
+    'rotation_range_z': [-180, 180],
+    'rotation_range_y': [-180, 180],
+    'rotation_range_x': [-180, 180]
+}
+
+
+# data preparation function
+def data_prep_function(data, patch_center, pixel_spacing, luna_origin, p_transform,
+                       p_transform_augment, mask_shape, **kwargs):
+    x = data_transforms.hu2normHU(data)
+    x, patch_annotation_tf = data_transforms.transform_patch3d(data=x,
+                                                               luna_annotations=None,
+                                                               patch_center=patch_center,
+                                                               p_transform=p_transform,
+                                                               p_transform_augment=p_transform_augment,
+                                                               pixel_spacing=pixel_spacing,
+                                                               luna_origin=luna_origin)
+
+    return x
+
+
+data_prep_function_train = partial(data_prep_function, p_transform_augment=p_transform_augment,
+                                   p_transform=p_transform,
+                                   mask_shape='sphere')
+data_prep_function_valid = partial(data_prep_function, p_transform_augment=None,
+                                   p_transform=p_transform,
+                                   mask_shape='sphere')
+
+# data iterators
+batch_size = 5
+nbatches_chunk = 5
+chunk_size = batch_size * nbatches_chunk
+
+train_valid_ids = utils.load_pkl(pathfinder.LUNA_VALIDATION_SPLIT_PATH)
+train_pids, valid_pids = train_valid_ids['train'], train_valid_ids['valid']
+
+train_data_iterator = data_iterators.Luna_DG_Elias(data_path=pathfinder.LUNA_DATA_PATH,
+                                                                    batch_size=chunk_size,
+                                                                    transform_params=p_transform,
+                                                                    data_prep_fun=data_prep_function_train,
+                                                                    rng=rng,
+                                                                    patient_ids=train_valid_ids['train'],
+                                                                    full_batch=True, random=True, infinite=True)
+
+valid_data_iterator = data_iterators.Luna_DG_Elias(data_path=pathfinder.LUNA_DATA_PATH,
+                                                                    batch_size=1,
+                                                                    transform_params=p_transform,
+                                                                    data_prep_fun=data_prep_function_valid,
+                                                                    rng=rng,
+                                                                    patient_ids=train_valid_ids['valid'],
+                                                                    full_batch=False, random=False, infinite=False)
+
+nchunks_per_epoch = train_data_iterator.nsamples / chunk_size
+max_nchunks = nchunks_per_epoch * 30
+
+validate_every = int(1. * nchunks_per_epoch)
+save_every = int(0.5 * nchunks_per_epoch)
+
+learning_rate_schedule = {
+    0: 1e-4,
+    int(max_nchunks * 0.5): 5e-5,
+    int(max_nchunks * 0.6): 4e-5,
+    int(max_nchunks * 0.7): 3e-5,
+    int(max_nchunks * 0.8): 2e-5,
+    int(max_nchunks * 0.9): 1e-6
+}
+
+# model
+conv3d = partial(dnn.Conv3DDNNLayer,
+                 filter_size=3,
+                 pad='same',
+                 W=nn.init.Orthogonal(),
+                 b=nn.init.Constant(0.01),
+                 nonlinearity=None)
+
+nl = partial(lasagne.layers.NonlinearityLayer,
+    nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
+
+max_pool3d = partial(dnn.MaxPool3DDNNLayer,
+                     pool_size=2)
+
+drop = lasagne.layers.DropoutLayer
+
+bn = lasagne.layers.batch_norm
+
+dense = partial(lasagne.layers.DenseLayer,
+    W=lasagne.init.Orthogonal('relu'),
+    b=lasagne.init.Constant(0.0),
+    nonlinearity=lasagne.nonlinearities.rectify)
+
+gp = lasagne.layers.GlobalPoolLayer
+
+def build_model():
+    l_in = nn.layers.InputLayer((None, 1,) + p_transform['patch_size'])
+    l_target = nn.layers.InputLayer((None, 1))
+
+    l = nl(bn(conv3d(l_in, num_filters=32)))
+    l = nl(bn(conv3d(l, num_filters=32)))
+    l = max_pool3d(l)
+    l = drop(l)
+
+    l = nl(bn(conv3d(l, num_filters=64)))
+    l = nl(bn(conv3d(l, num_filters=64)))
+    l = max_pool3d(l)
+    l = drop(l)
+
+    l = nl(bn(conv3d(l, num_filters=128)))
+    l = nl(bn(conv3d(l, num_filters=128)))
+    l = max_pool3d(l)
+    l = drop(l)
+
+    l = nl(bn(conv3d(l, num_filters=256)))
+    l = nl(bn(conv3d(l, num_filters=256)))
+    l = max_pool3d(l)
+    l = drop(l)
+
+
+    l = gp(l)
+
+    l_out = nn.layers.DenseLayer(l, num_units=2,
+                                 W=nn.init.Constant(0.),
+                                 nonlinearity=nn.nonlinearities.softmax)
+    
+
+    return namedtuple('Model', ['l_in', 'l_out', 'l_target'])(l_in, l_out, l_target)
+
+
+def build_objective(model, deterministic=False, epsilon=1e-12):
+    predictions = nn.layers.get_output(model.l_out)
+    targets = T.cast(T.flatten(nn.layers.get_output(model.l_target)), 'int32')
+    p = predictions[T.arange(predictions.shape[0]), targets]
+    p = T.clip(p,epsilon,1.)
+
+    loss = T.mean(T.log(p))
+    return -loss
+
+#todo
+def sparse_categorical_crossentropy(output, target, from_logits=False):
+    target = T.cast(T.flatten(target), 'int32')
+    target = T.extra_ops.to_one_hot(target, nb_class=output.shape[-1])
+    target = reshape(target, shape(output))
+    return categorical_crossentropy(output, target, from_logits)
+
+
+
+
+def build_updates(train_loss, model, learning_rate):
+    updates = nn.updates.adam(train_loss, nn.layers.get_all_params(model.l_out,trainable=True), learning_rate)
+    return updates
+
+
