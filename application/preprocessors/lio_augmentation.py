implemented a quick and dirty fix for the translation before rotation bug added small features to roi.py

application/preprocessors/lio_augmentation.py
@@ -39,7 +39,39 @@ NORMOFFSET = - MIN_HU*NORMSCALE - PIXEL_MEAN
 def normalize_and_center(x): return x*NORMSCALE + NORMOFFSET
 
 
+def lio_augment_positive_only(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p,additional_translation, interp_order=1, cval=MIN_HU):
+
+    
+    input_shape = np.asarray(volume.shape, np.float)
+    pixel_spacing = np.asarray(pixel_spacing, np.float)
+    output_shape = np.asarray(output_shape, np.float)
+    norm_patch_shape = np.asarray(norm_patch_shape, np.float)
+
+    norm_shape = input_shape * pixel_spacing
+    # this will stretch in some dimensions, but the stretch is consistent across samples
+    patch_shape = norm_shape * output_shape / norm_patch_shape
+    # else, use this: patch_shape = norm_shape * np.min(output_shape / norm_patch_shape)
+
+    shift_center = affine_transform(translation=-input_shape / 2. - 0.5)
+    normscale = affine_transform(scale=norm_shape / input_shape)
+    patch_centered = affine_transform(translation=additional_translation)
+    augment = affine_transform(**augment_p)
+    patchscale = affine_transform(scale=patch_shape / norm_shape)
+    unshift_center = affine_transform(translation=output_shape / 2. - 0.5)
+
+    matrix = shift_center.dot(normscale).dot(patch_centered).dot(augment).dot(patchscale).dot(unshift_center)
+
+    output = apply_affine_transform(volume, matrix,
+                                    order=interp_order,
+                                    output_shape=output_shape.astype("int"),
+                                    cval=cval)
+    return output
+
+
+
 def lio_augment(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p, interp_order=1, cval=MIN_HU):
+
+    
     input_shape = np.asarray(volume.shape, np.float)
     pixel_spacing = np.asarray(pixel_spacing, np.float)
     output_shape = np.asarray(output_shape, np.float)
@@ -66,12 +98,15 @@ def lio_augment(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p
 
 
 def sample_augmentation_parameters(augm_param):
+
     augm = dict(augm_param)
+    
     augm["scale"] = [log_uniform(v) for v in augm_param["scale"]]
     augm["rotation"] = [uniform(v) for v in augm_param["rotation"]]
     augm["shear"] = [uniform(v) for v in augm_param["shear"]]
     augm["translation"] = [uniform(v) for v in augm_param["translation"]]
     augm["reflection"] = [bernoulli(v) for v in augm_param["reflection"]]
+    
     return augm
 
 
@@ -175,27 +210,31 @@ class AugmentOnlyPositive(LioAugment):
                 volume = sample[INPUT][tag]
 
                 augment_p = dict(orig_augment)
-                augment_p["translation"] = augment_p["translation"] + (0.5*np.array(volume.shape)-labelloc)*spacing
+                #augment_p["translation"] = augment_p["translation"] + (0.5*np.array(volume.shape)-labelloc)*spacing
 
-                sample[INPUT][tag] = lio_augment(
-                    volume=volume,
-                    pixel_spacing=spacing,
-                    output_shape=self.output_shape,
-                    norm_patch_shape=self.norm_patch_size,
-                    augment_p=augment_p
-                )
-            elif tag in sample[OUTPUT]:
-                volume = sample[OUTPUT][tag]
-
-                augment_p = dict(orig_augment)
-                augment_p["translation"] = augment_p["translation"] + (0.5*np.array(volume.shape)-labelloc)*spacing
-
-                sample[OUTPUT][tag] = lio_augment(
+                sample[INPUT][tag] = lio_augment_positive_only(
                     volume=volume,
                     pixel_spacing=spacing,
                     output_shape=self.output_shape,
                     norm_patch_shape=self.norm_patch_size,
                     augment_p=augment_p,
+                    additional_translation=(0.5*np.array(volume.shape)-labelloc)*spacing                    
+                )
+            elif tag in sample[OUTPUT]:
+                volume = sample[OUTPUT][tag]
+
+                augment_p = dict(orig_augment)
+                #augment_p["translation"] = augment_p["translation"] + (0.5*np.array(volume.shape)-labelloc)*spacing
+
+                
+                
+                sample[OUTPUT][tag] = lio_augment_positive_only(
+                    volume=volume,
+                    pixel_spacing=spacing,
+                    output_shape=self.output_shape,
+                    norm_patch_shape=self.norm_patch_size,
+                    augment_p=augment_p,
+                    additional_translation=(0.5*np.array(volume.shape)-labelloc)*spacing,                    
                     cval=0.0
                 )
             else:

utils/transformation_3d.py
@@ -14,6 +14,13 @@ def affine_transform(scale=None, rotation=None, translation=None, shear=None, re
     """
     rotation and shear in degrees
     """
+    # print "***************************************************".format(scale)
+    # print "scale: {}".format(scale)
+    # print "rotation: {}".format(rotation)
+    # print "translation: {}".format(translation)
+    # print "shear: {}".format(shear)
+    # print "reflection: {}".format(reflection)
+    
     matrix = np.eye(4)
 
     if not translation is None:
@@ -70,7 +77,11 @@ def affine_transform(scale=None, rotation=None, translation=None, shear=None, re
     mz[0, 1] = -sin[2]
     mz[1, 1] = cos[2]
 
-    return matrix.dot(mx).dot(my).dot(mz)
+#    print "matrix: {}".format(matrix)
+    
+    #return matrix.dot(mx).dot(my).dot(mz)
+    return mx.dot(my).dot(mz).dot(matrix)
+    #return mz.dot(my).dot(mx).dot(matrix)
 
 def test_transformation_3d():
     import numpy as np