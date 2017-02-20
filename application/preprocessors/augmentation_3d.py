cleaned up the fix for the rotation prior to translation problem of our segmentation unets also introduced in in the newer augmentation preprocessors (augmentation_only_positive.py, augmentation_3d.py) added a demo configuration (segnet_demo_1) that proves that the fix works

application/preprocessors/augmentation_3d.py
@@ -50,7 +50,15 @@ def sample_augmentation_parameters(augm):
     return new_augm
 
 
-def augment_3d(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p, **kwargs):
+def augment_3d(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p,center_to_shift=None, **kwargs):
+
+
+    
+    if center_to_shift is None:
+        #if no explicit center has been given, just center the image at the origin
+        center_to_shift=-(input_shape / 2. - 0.5)
+
+    
     input_shape = np.asarray(volume.shape, np.float)
     pixel_spacing = np.asarray(pixel_spacing, np.float)
     output_shape = np.asarray(output_shape, np.float)
@@ -61,7 +69,7 @@ def augment_3d(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p,
     patch_shape = norm_shape * output_shape / norm_patch_shape
     # else, use this: patch_shape = norm_shape * np.min(output_shape / norm_patch_shape)
 
-    shift_center = affine_transform(translation=- (input_shape / 2. - 0.5))
+    shift_center = affine_transform(translation=center_to_shift)
     normscale = affine_transform(scale=norm_shape / input_shape)
     augments = affine_transform(**augment_p)
     patchscale = affine_transform(scale=patch_shape / norm_shape)

application/preprocessors/lio_augmentation.py
@@ -39,8 +39,43 @@ NORMOFFSET = - MIN_HU*NORMSCALE - PIXEL_MEAN
 def normalize_and_center(x): return x*NORMSCALE + NORMOFFSET
 
 
-def lio_augment_positive_only(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p,additional_translation, interp_order=1, cval=MIN_HU):
+# def lio_augment_positive_only(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p,additional_translation, interp_order=1, cval=MIN_HU):
 
+    
+#     input_shape = np.asarray(volume.shape, np.float)
+#     pixel_spacing = np.asarray(pixel_spacing, np.float)
+#     output_shape = np.asarray(output_shape, np.float)
+#     norm_patch_shape = np.asarray(norm_patch_shape, np.float)
+
+#     norm_shape = input_shape * pixel_spacing
+#     # this will stretch in some dimensions, but the stretch is consistent across samples
+#     patch_shape = norm_shape * output_shape / norm_patch_shape
+#     # else, use this: patch_shape = norm_shape * np.min(output_shape / norm_patch_shape)
+
+#     shift_center = affine_transform(translation=-input_shape / 2. - 0.5)
+#     normscale = affine_transform(scale=norm_shape / input_shape)
+#     patch_centered = affine_transform(translation=additional_translation)
+#     augment = affine_transform(**augment_p)
+#     patchscale = affine_transform(scale=patch_shape / norm_shape)
+#     unshift_center = affine_transform(translation=output_shape / 2. - 0.5)
+
+#     matrix = shift_center.dot(normscale).dot(patch_centered).dot(augment).dot(patchscale).dot(unshift_center)
+
+#     output = apply_affine_transform(volume, matrix,
+#                                     order=interp_order,
+#                                     output_shape=output_shape.astype("int"),
+#                                     cval=cval)
+#     return output
+
+
+
+def lio_augment(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p, interp_order=1,center_to_shift=None, cval=MIN_HU):
+
+
+    if center_to_shift is None:
+        #if no explicit center has been given, just center the image at the origin
+        center_to_shift=-input_shape / 2. - 0.5
+    
     
     input_shape = np.asarray(volume.shape, np.float)
     pixel_spacing = np.asarray(pixel_spacing, np.float)
@@ -52,37 +87,7 @@ def lio_augment_positive_only(volume, pixel_spacing, output_shape, norm_patch_sh
     patch_shape = norm_shape * output_shape / norm_patch_shape
     # else, use this: patch_shape = norm_shape * np.min(output_shape / norm_patch_shape)
 
-    shift_center = affine_transform(translation=-input_shape / 2. - 0.5)
-    normscale = affine_transform(scale=norm_shape / input_shape)
-    patch_centered = affine_transform(translation=additional_translation)
-    augment = affine_transform(**augment_p)
-    patchscale = affine_transform(scale=patch_shape / norm_shape)
-    unshift_center = affine_transform(translation=output_shape / 2. - 0.5)
-
-    matrix = shift_center.dot(normscale).dot(patch_centered).dot(augment).dot(patchscale).dot(unshift_center)
-
-    output = apply_affine_transform(volume, matrix,
-                                    order=interp_order,
-                                    output_shape=output_shape.astype("int"),
-                                    cval=cval)
-    return output
-
-
-
-def lio_augment(volume, pixel_spacing, output_shape, norm_patch_shape, augment_p, interp_order=1, cval=MIN_HU):
-
-    
-    input_shape = np.asarray(volume.shape, np.float)
-    pixel_spacing = np.asarray(pixel_spacing, np.float)
-    output_shape = np.asarray(output_shape, np.float)
-    norm_patch_shape = np.asarray(norm_patch_shape, np.float)
-
-    norm_shape = input_shape * pixel_spacing
-    # this will stretch in some dimensions, but the stretch is consistent across samples
-    patch_shape = norm_shape * output_shape / norm_patch_shape
-    # else, use this: patch_shape = norm_shape * np.min(output_shape / norm_patch_shape)
-
-    shift_center = affine_transform(translation=-input_shape / 2. - 0.5)
+    shift_center = affine_transform(translation=center_to_shift)
     normscale = affine_transform(scale=norm_shape / input_shape)
     augment = affine_transform(**augment_p)
     patchscale = affine_transform(scale=patch_shape / norm_shape)
@@ -212,13 +217,13 @@ class AugmentOnlyPositive(LioAugment):
                 augment_p = dict(orig_augment)
                 #augment_p["translation"] = augment_p["translation"] + (0.5*np.array(volume.shape)-labelloc)*spacing
 
-                sample[INPUT][tag] = lio_augment_positive_only(
+                sample[INPUT][tag] = lio_augment(
                     volume=volume,
                     pixel_spacing=spacing,
                     output_shape=self.output_shape,
                     norm_patch_shape=self.norm_patch_size,
                     augment_p=augment_p,
-                    additional_translation=(0.5*np.array(volume.shape)-labelloc)*spacing                    
+                    center_to_shift= - labelloc                     
                 )
             elif tag in sample[OUTPUT]:
                 volume = sample[OUTPUT][tag]
@@ -228,13 +233,13 @@ class AugmentOnlyPositive(LioAugment):
 
                 
                 
-                sample[OUTPUT][tag] = lio_augment_positive_only(
+                sample[OUTPUT][tag] = lio_augment(
                     volume=volume,
                     pixel_spacing=spacing,
                     output_shape=self.output_shape,
                     norm_patch_shape=self.norm_patch_size,
                     augment_p=augment_p,
-                    additional_translation=(0.5*np.array(volume.shape)-labelloc)*spacing,                    
+                    center_to_shift= - labelloc,                    
                     cval=0.0
                 )
             else: