3c91d3df34760b510debfa390c8f88f84316b34c
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-03-13 18:30:59 +0100

    lung segmentation is working good

--- lung_segmentation.py
+++ lung_segmentation.py
@@ -28,8 +28,9 @@ def segment_HU_scan_frederic(x, threshold=-350):
         # fill the body part
         filled = scipy.ndimage.binary_fill_holes(binary_part[iz])  # fill body
         filled_borders_mask = skimage.morphology.binary_erosion(filled, selem1)
-
         mask[iz] *= filled_borders_mask
+
+
         mask[iz] = skimage.morphology.closing(mask[iz], selem2)
         mask[iz] = skimage.morphology.erosion(mask[iz], selem3)
         mask[iz] = mask[iz] < threshold
@@ -42,45 +43,61 @@ def segment_HU_scan_elias(x, threshold=-350, pid='test'):
     binary_part = mask > threshold
     selem1 = skimage.morphology.disk(8)
     selem2 = skimage.morphology.disk(2)
-    selem3 = skimage.morphology.disk(9)
+    selem3 = skimage.morphology.disk(10)
 
     for iz in xrange(mask.shape[0]):
         # fill the body part
         filled = scipy.ndimage.binary_fill_holes(binary_part[iz])  # fill body
         filled_borders_mask = skimage.morphology.binary_erosion(filled, selem1)
-
         mask[iz] *= filled_borders_mask
+
         mask[iz] = skimage.morphology.closing(mask[iz], selem2)
-        mask[iz] = skimage.morphology.erosion(mask[iz], selem3)
         mask[iz] = mask[iz] < threshold
 
-    #discard disconnected regions
+    # params
+    overlap_treshold = 7
+    ratio_overlap_treshold = 0.015
+
+    #discard disconnected regions, start at the middle slice and go to the head
     for iz in range(mask.shape[0]/2, mask.shape[0]-1):
         overlap = mask[iz] * mask[iz+1] 
         label_image = skimage.measure.label(mask[iz+1])
+        print 'iz', iz
         for idx, region in enumerate(skimage.measure.regionprops(label_image)):
             total_overlap = 0
             for coordinates in region.coords:                
                 total_overlap += overlap[coordinates[0], coordinates[1]]
-            if total_overlap < 2:
-                print 'region', idx, 'in slice z=', iz-1, 'has no overlap and will be discarded'
+
+            ratio_overlap = 1.*total_overlap/region.area
+            print 'region', idx, ', t_overlap', total_overlap, ', r_overlap ', ratio_overlap, ', area ', region.area, ', center', np.round(region.centroid)
+            if total_overlap < overlap_treshold or ratio_overlap < ratio_overlap_treshold:
+                print 'region', idx, 'in slice z=', iz-1, 'has a low overlap (', total_overlap, ratio_overlap, ') and will be discarded'
                 for coordinates in region.coords: 
                     mask[iz+1, coordinates[0], coordinates[1]] = 0
 
-
+    #discard disconnected regions, start at the middle slice and go to the head
     for iz in range(mask.shape[0]/2,0,-1 ):
         overlap = mask[iz] * mask[iz-1] 
         label_image = skimage.measure.label(mask[iz-1])
+        print 'iz', iz
         for idx, region in enumerate(skimage.measure.regionprops(label_image)):
             total_overlap = 0
             for coordinates in region.coords:                
                 total_overlap += overlap[coordinates[0], coordinates[1]]
-            if total_overlap < 2:
-                print 'region', idx, 'in slice z=', iz-1, 'has no overlap and will be discarded'
+            ratio_overlap = 1.*total_overlap/region.area
+            print 'region', idx, ', t_overlap', total_overlap, ', r_overlap ', ratio_overlap, ', area ', region.area, ', center', np.round(region.centroid)
+            if total_overlap < overlap_treshold or ratio_overlap < ratio_overlap_treshold:
+                print 'region', idx, 'in slice z=', iz-1, 'has a low overlap (', total_overlap, ratio_overlap, ') and will be discarded'
                 for coordinates in region.coords: 
                     mask[iz-1, coordinates[0], coordinates[1]] = 0
 
-    #utils_plots.plot_all_slices(x, mask, pid, './plots/')
+
+    #erode out the blood vessels and the borders of the lung for a cleaner mask
+    for iz in xrange(mask.shape[0]):
+        mask[iz] = skimage.morphology.binary_dilation(mask[iz], selem3)
+
+
+    utils_plots.plot_all_slices(x, mask, pid, './plots/')
 
     return mask
 
