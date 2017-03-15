29aae62d58e54906f97a46c50405942b319fc037
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-03-15 15:38:41 +0100

    adding lung segm ira v2

--- lung_segmentation.py
+++ lung_segmentation.py
@@ -104,12 +104,57 @@ def segment_HU_scan_elias(x, threshold=-350, pid='test', plot=False, verbose=Fal
 
 
     if plot:
-        utils_plots.plot_all_slices(x, mask, pid, './plots/')
+        utils_plots.plot_all_slices(x, mask, pid, './plots/segment_HU_scan_elias/')
 
     return mask
 
 
+def segment_HU_scan_ira2(x, threshold=-350, pid='test', plot=False):
+    mask = np.asarray(x < threshold, dtype='int8')
 
+    for zi in xrange(mask.shape[0]):
+        skimage.segmentation.clear_border(mask[zi, :, :], in_place=True)
+
+    label_image = skimage.measure.label(mask)
+    region_props = skimage.measure.regionprops(label_image)
+    sorted_regions = sorted(region_props, key=lambda x: x.area, reverse=True)
+    lung_label = sorted_regions[0].label
+    lung_mask = np.asarray((label_image == lung_label), dtype='int8')
+
+    # convex hull mask
+    lung_mask_convex = np.zeros_like(lung_mask)
+    for i in range(lung_mask.shape[2]):
+        if np.any(lung_mask[:, :, i]):
+            lung_mask_convex[:, :, i] = skimage.morphology.convex_hull_image(lung_mask[:, :, i])
+
+    # old mask inside the convex hull
+    mask *= lung_mask_convex
+    label_image = skimage.measure.label(mask)
+    region_props = skimage.measure.regionprops(label_image)
+    sorted_regions = sorted(region_props, key=lambda x: x.area, reverse=True)
+    print len(sorted_regions)
+
+    for r in sorted_regions[1:]:
+        print r.area, r.label
+        if r.area > 10:
+            # make an image only containing that region
+            label_image_r = label_image == r.label
+            # grow the mask
+            label_image_r = scipy.ndimage.binary_dilation(label_image_r,
+                                                          structure=scipy.ndimage.generate_binary_structure(3, 2))
+            # compute the overlap with true lungs
+            overlap = label_image_r * lung_mask
+            if not np.any(overlap):
+                print 'REMOVE'
+                for i in range(label_image_r.shape[0]):
+                    if np.any(label_image_r[i]):
+                        label_image_r[i] = skimage.morphology.convex_hull_image(label_image_r[i])
+                lung_mask_convex *= 1 - label_image_r
+
+    if plot:
+        utils_plots.plot_all_slices(x, lung_mask_convex, pid, './plots/segment_HU_scan_ira2/')
+
+    return lung_mask_convex
 
 
 
