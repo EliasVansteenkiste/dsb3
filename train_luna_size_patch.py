c79ae5e6663ced1f749984f1c9a5462d0b244d35
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-03-22 23:12:16 +0100

    bugfix in print

--- train_luna_size_patch.py
+++ train_luna_size_patch.py
@@ -148,7 +148,7 @@ for chunk_idx, (x_chunk_train, y_chunk_train, id_train) in izip(chunk_idxs, buff
         # calculate mean train loss since the last validation phase
         mean_train_loss = np.mean(tmp_losses_train)
         mean_train_loss2 = np.mean(tmp_losses_train2)
-        print 'Mean train loss: %7f, %7f' % mean_train_loss, mean_train_loss2
+        print 'Mean train loss: %7f, %7f' % (mean_train_loss, mean_train_loss2)
         losses_eval_train.append(mean_train_loss)
         losses_eval_train2.append(mean_train_loss2)
         tmp_losses_train = []
