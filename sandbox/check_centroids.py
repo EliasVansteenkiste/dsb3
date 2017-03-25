82316d0f5d63625fd6f7d9d4358926ece275bb57
fgodin <frederic.godin@ugent.be>
2017-03-25 17:57:56 +0100

    final script malignance

--- sandbox/check_centroids.py
+++ sandbox/check_centroids.py
@@ -2,6 +2,47 @@ import os
 import cPickle
 import pathfinder
 import utils_lung
+import numpy as np
+
+def read_mhd_file(path):
+    # SimpleITK has trouble with multiprocessing :-/
+    import SimpleITK as sitk    # sudo pip install --upgrade pip; sudo pip install SimpleITK
+    itk_data = sitk.ReadImage(path.encode('utf-8'))
+    pixel_data = sitk.GetArrayFromImage(itk_data)
+    origin = np.array(list(itk_data.GetOrigin()))
+    spacing = np.array(list(itk_data.GetSpacing()))
+    return pixel_data, origin, spacing
+def load_patient_data(path):
+    result = dict()
+    pixel_data, origin, spacing = read_mhd_file(path)
+    result["pixeldata"] = pixel_data.T  # move from zyx to xyz
+    result["origin"] = origin  # move from zyx to xyz
+    print origin
+    result["spacing"] = spacing  # move from zyx to xyz
+    print spacing
+    return result
+def world_to_voxel_coordinates(world_coord, origin, spacing):
+    stretched_voxel_coord = np.absolute(world_coord - origin)
+    voxel_coord = stretched_voxel_coord / spacing
+    return voxel_coord
+
+def transform_nodules_vox_coos(nodules, origin, spacing):
+    coos_nodules = []
+    diameters_original = []
+    for nodule in nodules:
+        coordinates = world_to_voxel_coordinates(nodule[0:3], origin, spacing)
+        coos_nodules.append(coordinates)
+        diameters_original.append(nodule[3])
+    return coos_nodules, diameters_original
+
+def voxel_to_world_coordinates(voxel_coord, origin, spacing):
+
+    stretched_voxel_coord = voxel_coord *spacing
+    world_coord = np.absolute(stretched_voxel_coord + origin)
+    return world_coord
+
+def L2(a,b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)**(0.5)
 
 dumpdir = "/home/frederic/kaggle-dsb3/data/luna/nodule_annotations"
 
@@ -14,5 +55,15 @@ for f_name in os.listdir(dumpdir):
     patient = cPickle.load(open(os.path.join(dumpdir,f_name),"rb"))
 
     if pid in anno:
-        t = anno[pid]
-        print()
+
+        luna_nodules = anno[pid]
+        for doctor in patient:
+            for nodule in doctor:
+                for ln in luna_nodules:
+                    if "centroid_xyz" in nodule:
+                        if L2(ln[0:3],nodule["centroid_xyz"][::-1]) < 5:
+                            print("Very close")
+                        else:
+                            print("Not so close")
+                    else:
+                        print("Error")
--- sandbox/malignance_annotations.py
+++ sandbox/malignance_annotations.py
@@ -3,6 +3,34 @@ import cPickle
 import os
 from bs4 import BeautifulSoup
 import numpy
+import numpy as np
+
+def read_mhd_file(path):
+    # SimpleITK has trouble with multiprocessing :-/
+    import SimpleITK as sitk    # sudo pip install --upgrade pip; sudo pip install SimpleITK
+    itk_data = sitk.ReadImage(path.encode('utf-8'))
+    pixel_data = sitk.GetArrayFromImage(itk_data)
+    origin = np.array(list(itk_data.GetOrigin()))
+    spacing = np.array(list(itk_data.GetSpacing()))
+    return pixel_data, origin, spacing
+
+def load_patient_data(path):
+    result = dict()
+    pixel_data, origin, spacing = read_mhd_file(path)
+    result["pixeldata"] = pixel_data.T  # move from zyx to xyz
+    result["origin"] = origin  # move from zyx to xyz
+    #print origin
+    result["spacing"] = spacing  # move from zyx to xyz
+    #print spacing
+    return result
+
+def voxel_to_world_coordinates(voxel_coord, origin, spacing):
+
+    stretched_voxel_coord = voxel_coord *spacing
+    world_coord = np.absolute(stretched_voxel_coord + origin)
+    return world_coord
+
+
 
 rootdir = "/home/frederic/kaggle-dsb3/data/luna_xml"
 dumpdir = "/home/frederic/kaggle-dsb3/data/luna/nodule_annotations"
@@ -50,11 +78,13 @@ for rootd, subdirs, files in os.walk(rootdir):
                     for reading_session in root.findall("{http://www.nih.gov}readingSession"):
 
                         reading_session_list =[]
+                        chars_found = 0
                         for unblinded_nodule in reading_session.findall("{http://www.nih.gov}unblindedReadNodule"):
 
                             characteristics = unblinded_nodule.findall("{http://www.nih.gov}characteristics")
 
                             if len(characteristics)>0:
+                                chars_found+=1
                                 char_dict = {}
                                 for el in characteristics[0]._children:
                                     char_dict[el.tag[20:]]=float(el.text.strip())
@@ -75,14 +105,29 @@ for rootd, subdirs, files in os.walk(rootdir):
 
                                 nodule = {}
                                 nodule["characteristics"] = char_dict
-                                nodule["rois"]=xyz
+                                #nodule["rois"]=xyz
                                 nodule["centroid"]=(numpy.mean([x[0] for x in xyz]),
                                                     numpy.mean([x[1] for x in xyz]),
                                                     numpy.mean([x[2] for x in xyz]),
                                                     )
 
+
+
                                 reading_session_list.append(nodule)
                         sessions.append(reading_session_list)
+
+                    if chars_found>0:
+
+                        for s in sessions:
+                            for nodule in s:
+
+                                patient_mhd = load_patient_data(
+                                    '/home/frederic/kaggle-dsb3/data/luna/dataset/' + ser_id + '.mhd')
+
+                                xy_tf = voxel_to_world_coordinates(nodule["centroid"][0:2], patient_mhd['origin'][0:2],
+                                                                   patient_mhd['spacing'][0:2])
+                                nodule["centroid_xyz"] = (xy_tf[0], xy_tf[1], nodule["centroid"][2])
+                                nodule.pop('centroid', None)
 
                     with open(os.path.join(dumpdir,str(ser_id)+".pkl"),"wb") as f:
                         cPickle.dump(sessions,f)
