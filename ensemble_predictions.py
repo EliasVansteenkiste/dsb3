299ac2a7fd5ee78fb88ecb8a00198fa3d85e31d0
Unknown <andreas.verleysen@gmail.com>
2017-03-28 18:06:54 +0200

    CV in ensembles
    now do something with it

--- ensemble_predictions.py
+++ ensemble_predictions.py
@@ -7,6 +7,7 @@ import scipy
 import theano
 import theano.tensor as T
 import collections
+from sklearn.model_selection import StratifiedKFold
 
 import evaluate_submission
 import pathfinder
@@ -15,7 +16,6 @@ import utils_lung
 import os.path as path
 import os
 
-CONFIGS = ['dsb_a02_c3_s1e_p8a1-20170320-171033', 'dsb_a03_c3_s1e_p8a1-20170321-154250']
 CONFIGS = ['dsb_a02_c3_s1e_p8a1', 'dsb_a03_c3_s1e_p8a1']
 expid = utils.generate_expid('ensemble')
 
@@ -52,7 +52,7 @@ def get_predictions_of_config(config_name, which_set):
 
     output_pkl_file = outputs_path + '/%s-%s.pkl' % (expid, which_set)
     preds = utils.load_pkl(output_pkl_file)  # pid2prediction
-    preds = collections.OrderedDict(preds.iteritems())
+    preds = collections.OrderedDict(sorted(preds.items()))
     return preds
 
 
@@ -61,7 +61,8 @@ def load_validation_labels():
     valid_pids = train_valid_ids['validation']
     id2label = utils_lung.read_labels(pathfinder.LABELS_PATH)
 
-    return {pid: id2label[pid] for pid in sorted(valid_pids)}
+    labels = {pid: id2label[pid] for pid in sorted(valid_pids)}
+    return collections.OrderedDict(sorted(labels.items()))
 
 
 def sanity_check(valid_set_predictions, valid_set_labels):
@@ -98,80 +99,84 @@ def optimize_weights(predictions, labels):
     """
     print 'Optimizing weights...'
     # weights = simple_average(predictions.keys())
-    weights = optimal_linear_weights(predictions, labels)
+    X = predictions_dict_to_3d_array(predictions, labels)
+    y = np.array(labels.values())
+
+    # k fold cv
+    n_folds = 5
+    skf = StratifiedKFold(n_splits=n_folds, random_state=0)
+    cv_result = []
+    for train_index, test_index in skf.split(np.zeros(y.shape[0]), y):
+        X_train, X_test = X[:, train_index, :], X[:, test_index, :]
+        y_train, y_test = y[train_index], y[test_index]
+
+        weights, loss = optimal_linear_weights(X_train, np.array(one_hot(y_train)))
+
+        y_test_pred = np.zeros(len(test_index))
+        for i, weight in enumerate(weights):
+            y_test_pred += X_test[i, :, 1] * weights[i]  # this can probably be replaced with a tensor dot product
+
+        valid_loss = utils_lung.log_loss(y_test, y_test_pred)
+        cv_result.append({
+            'weights': weights,
+            'training_loss': loss,
+            'validation_loss': valid_loss,
+            'training_idx': train_index
+        })
+
+    # TODO do something with CV results!
+    print cv_result
+
+    weights, loss = optimal_linear_weights(X, np.array(one_hot(y)))
+    print 'Optimal weights'
+    config2weights = {}
+    for model_nr in range(len(predictions.keys())):
+        config = predictions.keys()[model_nr]
+        print 'Weight for config {} is {}'.format(config, weights[model_nr])
+        config2weights[config] = weights[model_nr]
+
     print 'Ensemble will use following weights distribution:'
-    print weights
-    return weights
+    print config2weights
+    return config2weights
 
 
-def log_loss(y, t, eps=1e-15):
+def optimal_linear_weights(predictions_stack, targets):
     """
-    cross entropy loss, summed over classes, mean over batches
+
+    :param predictions_stack:  predictions as numpy array with shape [num_configs x num_patients x 2]
+    :param targets: target labels as one hot encoded 2D array with shape [num_patients x 2]
+    :return:
     """
-    y = T.clip(y, eps, 1 - eps)
-    loss = -T.sum(t * T.log(y)) / y.shape[0].astype(theano.config.floatX)
-    return loss
+    X = theano.shared(predictions_stack.astype(theano.config.floatX))  # [num_configs x num_patients x 2]
+    t = theano.shared(targets)
+    W = T.vector('W')
+    s = T.nnet.softmax(W).reshape((W.shape[0], 1, 1))
+    weighted_avg_predictions = T.sum(X * s, axis=0)  # T.tensordot(X, s, [[0], [0]])
+    error = log_loss(weighted_avg_predictions, t)
+    grad = T.grad(error, W)
+    f = theano.function([W], error)
+    g = theano.function([W], grad)
+    n_models = predictions_stack.shape[0]
+    w_init = np.zeros(n_models, dtype=theano.config.floatX)
+    out, loss, _ = scipy.optimize.fmin_l_bfgs_b(f, w_init, fprime=g, pgtol=1e-09, epsilon=1e-08, maxfun=10000)
+    weights = np.exp(out)
+    weights /= weights.sum()
+    return weights, loss
 
 
-def log_losses(y, t, eps=1e-15):
+def predictions_dict_to_3d_array(predictions, labels):
     """
-    cross entropy loss per example, summed over classes
+    :return: predictions as numpy array with shape [num_configs x num_patients x 2]
     """
-    y = T.clip(y, eps, 1 - eps)
-    losses = -T.sum(t * T.log(y), axis=1)
-    return losses
-
-
-def one_hot(vec, m=None):
-    if m is None:
-        m = int(np.max(vec)) + 1
-
-    return np.eye(m)[vec]
-
-
-def optimal_linear_weights(predictions, labels):
-    print "calculating optimal linear weights..."
     n_models = len(predictions.keys())
-    print ' amount models:', n_models
     n_patients = len(labels)
-
     predictions_stack = np.zeros((n_models, n_patients, 2))  # num_configs x num_patients x 2 categories
     for model_nr in range(n_models):
         config = predictions.keys()[model_nr]
         for patient_nr, patient_id in enumerate(labels.keys()):
             predictions_stack[model_nr, patient_nr, 0] = 1.0 - predictions[config][patient_id]
             predictions_stack[model_nr, patient_nr, 1] = predictions[config][patient_id]
-
-    predictions_stack = predictions_stack.astype(theano.config.floatX)
-    print 'predictions_stack dimension: ', predictions_stack.shape
-    targets = np.array(one_hot(labels.values()))  # .reshape((n_patients, 1))
-
-    X = theano.shared(predictions_stack)
-    t = theano.shared(targets)
-    W = T.vector('W')
-
-    s = T.nnet.softmax(W).reshape((W.shape[0], 1, 1))
-    weighted_avg_predictions = T.sum(X * s, axis=0)  # T.tensordot(X, s, [[0], [0]])
-    error = log_loss(weighted_avg_predictions, t)
-    grad = T.grad(error, W)
-
-    f = theano.function([W], error)
-    g = theano.function([W], grad)
-
-    w_init = np.zeros(n_models, dtype=theano.config.floatX)
-    out, loss, _ = scipy.optimize.fmin_l_bfgs_b(f, w_init, fprime=g, pgtol=1e-09, epsilon=1e-08, maxfun=10000)
-
-    weights = np.exp(out)
-    weights /= weights.sum()
-
-    print 'Optimal weights'
-    config2weights = {}
-    for model_nr in range(n_models):
-        config = predictions.keys()[model_nr]
-        print 'Weight for config {} is {}'.format(config, weights[model_nr])
-        config2weights[config] = weights[model_nr]
-
-    return config2weights
+    return predictions_stack
 
 
 def simple_average(configs):
@@ -202,7 +207,7 @@ def weighted_average(predictions, weights):
             else:
                 weighted_predictions[pid] = weighted_prediction
 
-    return weighted_predictions
+    return collections.OrderedDict(sorted(weighted_predictions.items()))
 
 
 def persist_predictions(y_test_pred, y_valid_pred):
@@ -232,6 +237,31 @@ def calc_test_performance(config_name, predictions):
     loss = evaluate_submission.leaderboard_performance(tmp_submission_file)
     os.remove(tmp_submission_file)
     return loss
+
+
+def log_loss(y, t, eps=1e-15):
+    """
+    cross entropy loss, summed over classes, mean over batches
+    """
+    y = T.clip(y, eps, 1 - eps)
+    loss = -T.sum(t * T.log(y)) / y.shape[0].astype(theano.config.floatX)
+    return loss
+
+
+def log_losses(y, t, eps=1e-15):
+    """
+    cross entropy loss per example, summed over classes
+    """
+    y = T.clip(y, eps, 1 - eps)
+    losses = -T.sum(t * T.log(y), axis=1)
+    return losses
+
+
+def one_hot(vec, m=None):
+    if m is None:
+        m = int(np.max(vec)) + 1
+
+    return np.eye(m)[vec]
 
 
 if __name__ == '__main__':
