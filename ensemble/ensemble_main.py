b77b4d5c71420985c5b40cd8000c9369e7169e89
Unknown <andreas.verleysen@gmail.com>
2017-04-05 14:31:15 +0200

    clean code

--- ensemble/ensemble_main.py
+++ ensemble/ensemble_main.py
@@ -1,7 +1,7 @@
 import data_loading
 import numpy as np
 import utils_ensemble
-import linear_ensemble
+import ensemble_models as em
 import utils_lung
 import collections
 import profile
@@ -20,50 +20,12 @@ CONFIGS = ['dsb_a04_c3ns2_mse_s5_p8a1', 'dsb_a07_c3ns3_mse_s5_p8a1', 'dsb_a08_c3
 CONFIGS += FG_CONFIGS
 OUTLIER_THRESHOLD = 0.20  # Disagreement threshold (%)
 DO_MAJORITY_VOTE = False
-VERBOSE = 0
-
-
-# TODO: to other file
-class Ensemble(object):
-    def __init__(self, models):
-        self.models = models
-        self.weights = None
-
-    def predict(self, X):
-        return self._weighted_average(X, self.weights)
-
-    def predict_one_sample(self, X):
-        assert len(X.values()[0]) == 1
-        return self._weighted_average(X, self.weights).values()[0]
-
-    def _weighted_average(self, predictions, weights):
-        """
-        Arithmetic average
-
-        :param predictions: (config_name -> (pid -> prediction) )
-        :param weights: (config_name -> (weight) )
-        :return  predictions as dict: (pid -> prediction)
-        """
-        if not self.__is_optimized():
-            raise ValueError('Cannot use ensemble if not optimized')
-        weighted_predictions = {}
-        for config_name, config_predictions in predictions.iteritems():
-            for pid, patient_prediction in config_predictions.iteritems():
-                weighted_prediction = patient_prediction * weights[config_name]
-                if pid in weighted_predictions:
-                    weighted_predictions[pid] += weighted_prediction
-                else:
-                    weighted_predictions[pid] = weighted_prediction
-
-        return collections.OrderedDict(sorted(weighted_predictions.items()))
-
-    def __is_optimized(self):
-        return self.weights is not None
+VERBOSE = True
 
 
 def ensemble(configs):
     X_valid, y_valid = load_data(configs, 'validation')
-    ensemble_model = linear_optimal_ensemble(X_valid, y_valid)
+    ensemble_model = em.linear_optimal_ensemble(X_valid, y_valid)
 
     X_test, y_test = load_data(configs, 'test')
     test_pids = y_test.keys()
@@ -84,7 +46,7 @@ def ensemble(configs):
 def majority_vote_rensemble_prediction(X_test, ensemble_pred, pid):
     configs_to_reuse = remove_outlier_configs(X_test, ensemble_pred, pid)
     X, y = load_data(configs_to_reuse, 'validation')
-    rensemble_model = linear_optimal_ensemble(X, y)
+    rensemble_model = em.linear_optimal_ensemble(X, y)
     test_sample = filter_set(X_test, pid, configs_to_reuse)
     final_pred = rensemble_model.predict_one_sample(test_sample)
     return final_pred
@@ -120,30 +82,6 @@ def load_data(configs, dataset_membership):
         return data_loading.load_test_set(configs)
     else:
         raise ValueError('Dude you drunk? No data set membership with name {} exists'.format(dataset_membership))
-
-
-def linear_optimal_ensemble(predictions, labels):
-    """
-
-    :type predictions: dict
-    :type labels: dict
-    :param predictions: (config_name -> (pid -> prediction) )
-    :param labels: ( (pid -> prediction) )
-    """
-
-    X = utils_ensemble.predictions_dict_to_3d_array(predictions)
-    y = np.array(labels.values())
-    weights = linear_ensemble.optimal_linear_weights(X, np.array(utils_ensemble.one_hot(y)))
-    if VERBOSE: print '\nOptimal weights'
-    config2weights = {}
-    for model_nr in range(len(predictions.keys())):
-        config = predictions.keys()[model_nr]
-        if VERBOSE: print 'Weight for config {} is {:0.2%}'.format(config, weights[model_nr])
-        config2weights[config] = weights[model_nr]
-
-    ensemble_model = Ensemble(predictions.keys())
-    ensemble_model.weights = config2weights
-    return ensemble_model
 
 
 # profile.run('ensemble(CONFIGS)')
---
+++ ensemble/ensemble_models.py
@@ -0,0 +1,93 @@
+import theano
+import theano.tensor as T
+
+from ensemble import utils_ensemble
+import numpy as np
+import scipy.optimize
+import collections
+
+VERBOSE = False
+
+
+class Ensemble(object):
+    def __init__(self, models):
+        self.models = models
+        self.weights = None
+
+    def predict(self, X):
+        return self._weighted_average(X, self.weights)
+
+    def predict_one_sample(self, X):
+        assert len(X.values()[0]) == 1
+        return self._weighted_average(X, self.weights).values()[0]
+
+    def _weighted_average(self, predictions, weights):
+        """
+        Arithmetic average
+
+        :param predictions: (config_name -> (pid -> prediction) )
+        :param weights: (config_name -> (weight) )
+        :return  predictions as dict: (pid -> prediction)
+        """
+        if not self.__is_optimized():
+            raise ValueError('Cannot use ensemble if not optimized')
+        weighted_predictions = {}
+        for config_name, config_predictions in predictions.iteritems():
+            for pid, patient_prediction in config_predictions.iteritems():
+                weighted_prediction = patient_prediction * weights[config_name]
+                if pid in weighted_predictions:
+                    weighted_predictions[pid] += weighted_prediction
+                else:
+                    weighted_predictions[pid] = weighted_prediction
+
+        return collections.OrderedDict(sorted(weighted_predictions.items()))
+
+    def __is_optimized(self):
+        return self.weights is not None
+
+
+def linear_optimal_ensemble(predictions, labels):
+    """
+
+    :type predictions: dict
+    :type labels: dict
+    :param predictions: (config_name -> (pid -> prediction) )
+    :param labels: ( (pid -> prediction) )
+    """
+
+    X = utils_ensemble.predictions_dict_to_3d_array(predictions)
+    y = np.array(labels.values())
+    weights = optimal_linear_weights(X, np.array(utils_ensemble.one_hot(y)))
+    if VERBOSE: print '\nOptimal weights'
+    config2weights = {}
+    for model_nr in range(len(predictions.keys())):
+        config = predictions.keys()[model_nr]
+        if VERBOSE: print 'Weight for config {} is {:0.2%}'.format(config, weights[model_nr])
+        config2weights[config] = weights[model_nr]
+
+    ensemble_model = Ensemble(predictions.keys())
+    ensemble_model.weights = config2weights
+    return ensemble_model
+
+
+def optimal_linear_weights(predictions_stack, targets):
+    """
+    :param predictions_stack:  predictions as numpy array with shape [num_configs x num_patients x 2]
+    :param targets: target labels as one hot encoded 2D array with shape [num_patients x 2]
+    :return:
+    """
+    X = theano.shared(predictions_stack.astype(theano.config.floatX))  # [num_configs x num_patients x 2]
+    t = theano.shared(targets)
+    W = T.vector('W')
+    s = T.nnet.softmax(W).reshape((W.shape[0], 1, 1))
+    weighted_avg_predictions = T.sum(X * s, axis=0)  # T.tensordot(X, s, [[0], [0]])
+    error = utils_ensemble.log_loss(weighted_avg_predictions, t)
+    grad = T.grad(error, W)
+    f = theano.function([W], error)
+    g = theano.function([W], grad)
+    n_models = predictions_stack.shape[0]
+    w_init = np.zeros(n_models, dtype=theano.config.floatX)
+    out, loss, _ = scipy.optimize.fmin_l_bfgs_b(f, w_init, fprime=g, pgtol=1e-09, epsilon=1e-08, maxfun=10000)
+    weights = np.exp(out)
+    weights /= weights.sum()
+    return weights
