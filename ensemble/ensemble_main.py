4bedca8fe2cd3026db1e6e7fece84865d4c06c01
Unknown <andreas.verleysen@gmail.com>
2017-04-05 18:45:00 +0200

    add configs of elias

--- ensemble/ensemble_main.py
+++ ensemble/ensemble_main.py
@@ -14,6 +14,8 @@ FG_CONFIGS = ['fgodin/' + config for config in
               ['dsb_af19lme_mal2_s5_p8a1', 'dsb_af25lme_mal2_s5_p8a1', 'dsb_af4_size6_s5_p8a1',
                'dsb_af5lme_mal2_s5_p8a1', 'dsb_af5_size6_s5_p8a1', 'dsb_af24lme_mal3_s5_p8a1']]
 
+EV_CONFIGS = ['eavsteen/' + config for config in ['dsb_a_eliasq1_mal2_s5_p8a1']]
+
 CONFIGS = ['dsb_a04_c3ns2_mse_s5_p8a1', 'dsb_a07_c3ns3_mse_s5_p8a1', 'dsb_a08_c3ns3_mse_s5_p8a1',
            'dsb_a11_m1zm_s5_p8a1', 'dsb_af25lmeaapm_mal2_s5_p8a1',
            'dsb_a_liolme16_c3_s2_p8a1', 'dsb_a_liolme32_c3_s5_p8a1', 'dsb_a_liox10_c3_s2_p8a1',
@@ -23,13 +25,13 @@ CONFIGS = ['dsb_a04_c3ns2_mse_s5_p8a1', 'dsb_a07_c3ns3_mse_s5_p8a1', 'dsb_a08_c3
            'dsb_a_liox7_c3_s2_p8a1', 'dsb_a_liox8_c3_s2_p8a1', 'dsb_a_liolunalme16_c3_s2_p8a1']
 
 FG_CONFIGS = ['fgodin/' + config for config in ['dsb_af25lme_mal2_s5_p8a1']]
-# GOOD_CONFIGS = ['dsb_af25lmeaapm_mal2_s5_p8a1', 'dsb_a_liolme32_c3_s5_p8a1', 'dsb_af25lmelr10-1_mal2_s5_p8a1',
-#                 'dsb_a_liox10_c3_s2_p8a1']
+GOOD_CONFIGS = ['dsb_af25lmeaapm_mal2_s5_p8a1', 'dsb_a_liolme32_c3_s5_p8a1', 'dsb_af25lmelr10-1_mal2_s5_p8a1',
+                'dsb_a_liox10_c3_s2_p8a1']
 
-CONFIGS = FG_CONFIGS + CONFIGS
-OUTLIER_THRESHOLD = 0.20  # Disagreement threshold (%)
+CONFIGS = FG_CONFIGS + CONFIGS + EV_CONFIGS
+OUTLIER_THRESHOLD = 0.10  # Disagreement threshold (%)
 DO_MAJORITY_VOTE = False
-DO_CV = False
+DO_CV = 0
 VERBOSE = True
 
 
@@ -60,7 +62,6 @@ def ensemble(configs):
         ensemble_pred = ensemble_model.predict_one_sample(test_sample)
         y_test_pred[pid] = majority_vote_rensemble_prediction(X_test, ensemble_pred,
                                                               pid) if DO_MAJORITY_VOTE else ensemble_pred
-
 
     evaluate_test_set_performance(y_test, y_test_pred)
 
