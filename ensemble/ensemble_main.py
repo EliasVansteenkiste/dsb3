12e3f3c417872b1deeff0503960dc26e7ae82fa7
Unknown <andreas.verleysen@gmail.com>
2017-04-07 11:06:22 +0200

    add some more configs to the ensemble

--- ensemble/ensemble_main.py
+++ ensemble/ensemble_main.py
@@ -25,7 +25,8 @@ CONFIGS = ['dsb_a04_c3ns2_mse_s5_p8a1', 'dsb_a07_c3ns3_mse_s5_p8a1', 'dsb_a08_c3
            'dsb_a_liox13_c3_s2_p8a1', 'dsb_a_liox14_c3_s2_p8a1', 'dsb_a_liox15_c3_s2_p8a1', 'dsb_a_liox6_c3_s2_p8a1',
            'dsb_a_liox7_c3_s2_p8a1', 'dsb_a_liox8_c3_s2_p8a1', 'dsb_a_liolunalme16_c3_s2_p8a1',
            'dsb_a_lionoclip_c3_s5_p8a1', 'dsb_a_liomse_c3_s5_p8a1', 'dsb_af25lmeo0_s5_p8a1',
-           'dsb_af25lmelr10-3_mal2_s5_p8a1']
+           'dsb_a_liomseresume_c3_s5_p8a1',
+           'dsb_af25lmelr10-3_mal2_s5_p8a1', 'dsb_a_liomix_c3_s5_p8a1', 'dsb_a_liomselunaresume_c3_s5_p8a1']
 
 FG_CONFIGS = ['fgodin/' + config for config in ['dsb_af25lme_mal2_s5_p8a1']]
 GOOD_CONFIGS = ['dsb_af25lmeaapm_mal2_s5_p8a1', 'dsb_a_liolme32_c3_s5_p8a1', 'dsb_af25lmelr10-1_mal2_s5_p8a1',
